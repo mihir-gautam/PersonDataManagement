7d0b896892ed8ed881528cdf33aa5805524650eb
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-19 17:53:48 +0530

    [Mihir] Add Welcome message

---
+++ PersonsDataManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonsDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Data Management Program");
+        }
+    }
+}
